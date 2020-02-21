f75b83e5321884e96d78352d588d428ffa99e4f9
ozawam <ozawa_m@eps.s.u-tokyo.ac.jp>
2020-02-21 10:27:57 +0900

    initial code

--- git_automatic.sh
+++ git_automatic.sh
@@ -4,4 +4,5 @@ MESSAGE=${1:-"add a subtle touch"}
 #git add Makefile git_automatic.sh problem.c
 git add *
 git commit -m "${MESSAGE}"
-git push origin master
+#git push origin master
+git push origin integrator
